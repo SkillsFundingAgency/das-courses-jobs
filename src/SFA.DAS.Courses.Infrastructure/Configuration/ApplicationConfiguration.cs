974f4333a4940407edee6e1bad7ec159fe0ea10e
Chris Woodcock <chris@humesoftware.com>
2025-01-20 16:22:04 +0000

    Fixed indentation

--- src/SFA.DAS.Courses.Infrastructure/Configuration/ApplicationConfiguration.cs
+++ src/SFA.DAS.Courses.Infrastructure/Configuration/ApplicationConfiguration.cs
@@ -4,9 +4,9 @@
     {
         public CoursesApiClientConfiguration CoursesApiClientConfiguration { get; set; }
         public string GitHubRepositoryName { get; set; }
-	    public string GitHubUserName { get; set; }
+        public string GitHubUserName { get; set; }
         public string GitHubEmail { get; set; }
-	    public string GitHubAccessToken {  get; set; }
+        public string GitHubAccessToken {  get; set; }
 
         public static string GitHubUrl => "https://api.github.com/repos/{0}/{1}/contents/";
     }
