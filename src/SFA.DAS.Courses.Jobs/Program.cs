affad92733341bd62803b4e182f5b0856a104f96
Chris Woodcock <chris@humesoftware.com>
2025-05-16 18:03:12 +0100

    Allow information logs to be captured

--- src/SFA.DAS.Courses.Jobs/Program.cs
+++ src/SFA.DAS.Courses.Jobs/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using SFA.DAS.Courses.Infrastructure.Configuration;
 using SFA.DAS.Courses.Jobs.Extensions;
 using SFA.DAS.Courses.Jobs.Services;
@@ -49,6 +50,19 @@ namespace SFA.DAS.Courses.Jobs
                         Console.WriteLine($"Exception in ConfigureService: {ex}");
                         throw;
                     }
+                })
+                .ConfigureLogging(logging =>
+                {
+                    // This rule filters logs to capture only warnings and errors, removing this rule will allow Information logs to be captured
+                    logging.Services.Configure<LoggerFilterOptions>(options =>
+                    {
+                        LoggerFilterRule? defaultRule = options.Rules.FirstOrDefault(rule => rule.ProviderName
+                            == "Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider");
+                        if (defaultRule is not null)
+                        {
+                            options.Rules.Remove(defaultRule);
+                        }
+                    });
                 })
                 .Build();
 
