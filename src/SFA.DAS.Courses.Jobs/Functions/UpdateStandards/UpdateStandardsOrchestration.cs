a3719162cfb106625ddfca1d345f161bfebeddea
Chris Woodcock <chris@humesoftware.com>
2025-05-19 11:08:21 +0100

    Log error for orchestration failure

--- src/SFA.DAS.Courses.Jobs/Functions/UpdateStandards/UpdateStandardsOrchestration.cs
+++ src/SFA.DAS.Courses.Jobs/Functions/UpdateStandards/UpdateStandardsOrchestration.cs
@@ -10,34 +10,42 @@ namespace SFA.DAS.Courses.Jobs.Functions.UpdateStandards
         public static async Task RunOrchestrator([OrchestrationTrigger] TaskOrchestrationContext context)
         {
             var logger = context.CreateReplaySafeLogger(typeof(UpdateStandardsOrchestration));
-            logger.LogInformation("{OrchestrationName} started", nameof(UpdateStandardsOrchestration));
 
-            var allStandards = await context.CallActivityAsync<Dictionary<string, string>>(nameof(GetAllStandardsActivity), new TaskOptions());
-            var standardsToProcess = new Dictionary<string, string>(allStandards);
-            var retryLimit = 3;
-            var attempt = 0;
+            try
+            {    
+                logger.LogInformation("{OrchestrationName} started", nameof(UpdateStandardsOrchestration));
 
-            while (standardsToProcess.Any() && attempt++ < retryLimit)
-            {
-                logger.LogInformation("Attempt {Attempt} - processing {Count} standards", attempt, standardsToProcess.Count);
+                var allStandards = await context.CallActivityAsync<Dictionary<string, string>>(nameof(GetAllStandardsActivity), new TaskOptions());
+                var standardsToProcess = new Dictionary<string, string>(allStandards);
+                var retryLimit = 3;
+                var attempt = 0;
 
-                var failedKeys = await context.CallActivityAsync<List<string>>(nameof(StoreGitHubBatchActivity), standardsToProcess);
+                while (standardsToProcess.Any() && attempt++ < retryLimit)
+                {
+                    logger.LogInformation("Attempt {Attempt} - processing {Count} standards", attempt, standardsToProcess.Count);
 
-                standardsToProcess = failedKeys.ToDictionary(key => key, key => allStandards[key]);
+                    var failedKeys = await context.CallActivityAsync<List<string>>(nameof(StoreGitHubBatchActivity), standardsToProcess);
 
-                logger.LogInformation("Attempt {Attempt} complete - {Failed} failures", attempt, failedKeys.Count);
+                    standardsToProcess = failedKeys.ToDictionary(key => key, key => allStandards[key]);
+
+                    logger.LogInformation("Attempt {Attempt} complete - {Failed} failures", attempt, failedKeys.Count);
+                }
+
+                if (standardsToProcess.Any())
+                {
+                    logger.LogWarning("Processing completed with {Count} failures after {Retries} retries", standardsToProcess.Count, retryLimit);
+                }
+                else
+                {
+                    logger.LogInformation("All standards processed successfully");
+                }
+
+                logger.LogInformation("{OrchestrationName} completed", nameof(UpdateStandardsOrchestration));
             }
-
-            if (standardsToProcess.Any())
+            catch(Exception ex)
             {
-                logger.LogWarning("Processing completed with {Count} failures after {Retries} retries", standardsToProcess.Count, retryLimit);
+                logger.LogError(ex, "{OrchestrationName} failed", nameof(UpdateStandardsOrchestration));
             }
-            else
-            {
-                logger.LogInformation("All standards processed successfully");
-            }
-
-            logger.LogInformation("{OrchestrationName} completed", nameof(UpdateStandardsOrchestration));
         }
     }
 }
